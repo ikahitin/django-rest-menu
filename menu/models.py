f0b1eb900dee96c21ac914a3871229b5ad2fa7d3
Ivan Kahitin <wiglegpro@gmail.com>
2020-12-21 22:18:46 +0200

    AWS S3 configured

--- menu/models.py
+++ menu/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120)
     description = models.TextField()
-    image_path = models.ImageField(upload_to='static/img/%Y/%m/%d', blank=True)
+    image_path = models.ImageField(upload_to='img', blank=True)
     price = models.IntegerField()
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     category_name = models.CharField(max_length=200, default=str(Category.objects.filter(id=1)))
